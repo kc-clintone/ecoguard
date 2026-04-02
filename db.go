8e1e5d24e03dbbed0d28cb15a0b6ff9eb886d726
Ashomondi <adhiamboa745@gmail.com>
2026-04-02 16:46:21 +0300

    the data used is in this file

---
+++ db.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func connectDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("MongoDB Connected")
+}
